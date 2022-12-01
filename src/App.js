e61ec07caa7914cfe1f7dd38e9b801035b3b687c
Natalie Salazar <natmsal33@gmail.com>
2022-11-30 22:51:38 -0800

    made style changes to theme

--- src/App.js
+++ src/App.js
@@ -1,41 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./NavBar.js";
-import twitterLogo from "./pictures/twitterLogo.png";
 import Tweets from "./Tweets.js";
 import CreatedBy from "./CreatedBy.js";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
-import Root, { loader as rootLoader } from "./routes/root";
-import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: rootLoader,
-    children: [
-      {
-        path: "contacts/:contactId",
-        element: <Contact />,
-      },
-    ],
-  },
-]);
 
 const App = () => {
   return (
     <div className="App">
-      {/* <div className="heading-navbar">
+      <div className="heading-navbar">
         <NavBar />
       </div>
       <div className="heading">
         <h1> Home -- About</h1>
-      </div> */}
-      {/* <Tweets /> */}
-      <RouterProvider router={router} />
-      {/* <CreatedBy /> */}
+      </div>
+      <Tweets />
+      <CreatedBy />
     </div>
   );
 };
